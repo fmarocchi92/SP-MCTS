d15e10a7a271d04b44d76ed0b56bffa3620634c1
Fabio Marocchi <f.marocchi.92@gmail.com>
2017-11-24 18:49:55 +0100

    Added binary and script to launch test

---
+++ MCTS2016/bin/Release/SameGame_SP-MCTS(3).bat
@@ -0,0 +1,2 @@
+::executable - game - const_C - const_D - maxMinutesPerSearch - maxIterations - restarts - seed - logfile - levelfile
+.\MCTS2016.exe samegame 4.31 96.67 600 1500 10 1 ./Log/log.txt ./Levels/samegame.txt
